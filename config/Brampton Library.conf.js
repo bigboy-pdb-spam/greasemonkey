1a6fde4ea56dc47e4a2a85df1d07dee6098e9814
Johnathon Fortier <dave@fakeemail.com>
2021-04-05 01:11:24 -0400

    Added config file template for Brampton Library

---
+++ config/Brampton Library.conf.js
@@ -0,0 +1,20 @@
+/*
+
+Script requires users variable containing library numbers and their
+ corresponding names and passwords to be declared as follows:
+
+let users = {
+  LIBRARY_NUMBER: {
+    name: NAME,
+    password: PASSWORD
+  },
+
+  ...
+
+  LIBRARY_NUMBER: {
+    name: NAME,
+    password: PASSWORD
+  }
+}
+
+*/
