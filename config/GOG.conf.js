73fb3075ed2986ed5a23dd16814d671eb6c62d10
Johnathon Fortier <dave@fakeemail.com>
2021-04-05 02:20:12 -0400

    Added config file template for GOG

---
+++ config/GOG.conf.js
@@ -0,0 +1,51 @@
+/*
+
+// IDs of games that I'm not interested in
+let uninterested = [
+  ID_NUMBER, // GAME NAME
+
+  ...
+
+  ID_NUMBER // GAME NAME
+];
+
+// Games that I might get
+let perhaps = [
+  ID_NUMBER, // GAME NAME
+
+  ...
+
+  ID_NUMBER // GAME NAME
+];
+
+// Styles for games based on their prices
+let price_ranges = [
+  // The default style
+  {
+    style: CSS_STYLE
+  },
+
+  // Style for price range
+  {
+    start: STARTING_PRICE_IN_WHOLE_DOLLARS,
+    end: ENDING_PRICE_IN_WHOLE_DOLLARS,
+    
+    style: CSS_STYLE
+  },
+
+  ...
+
+  // Style for price range
+  {
+    start: STARTING_PRICE_IN_WHOLE_DOLLARS,
+    end: ENDING_PRICE_IN_WHOLE_DOLLARS,
+    
+    style: CSS_STYLE
+  }
+];
+
+// The maximum price that I would buy a perhaps game at (up to that value and
+//  99 cents)
+let perhaps_max = 1; 
+ 
+*/
