b3749b1a5ae7a24d946c015b7de1dd4508f5a463
Johnathon Fortier <dave@fakeemail.com>
2021-04-05 01:49:23 -0400

    Added config file template for Prodigy

---
+++ config/Prodigy.conf.js
@@ -0,0 +1,20 @@
+/*
+
+Script requires users variable containing user login names and the names of the
+ people and passwords that correspond with them to be declared as follows:
+
+let users = {
+  USER_LOGIN_NAME: {
+    name: NAME,
+    password: PASSWORD
+  },
+
+  ...
+
+  USER_LOGIN_NAME: {
+    name: NAME,
+    password: PASSWORD
+  }
+}
+
+*/
