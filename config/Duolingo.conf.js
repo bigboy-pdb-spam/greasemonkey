047713baad141c6f89dcdfa8bed93df90d43df80
Johnathon Fortier <dave@fakeemail.com>
2021-04-05 06:22:02 -0400

    Added config file template for Duolingo

---
+++ config/Duolingo.conf.js
@@ -0,0 +1,20 @@
+/*
+
+Script requires users variable containing user email addresses and the names of
+ the people and passwords that correspond with them to be declared as follows:
+
+let users = {
+  USER_EMAIL: {
+    name: NAME,
+    password: PASSWORD
+  },
+
+  ...
+
+  USER_EMAIL: {
+    name: NAME,
+    password: PASSWORD
+  }
+}
+
+*/
